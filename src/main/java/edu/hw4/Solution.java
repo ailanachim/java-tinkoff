026f32b2654315efae649ed7c6b8096d7183157c
Ailana <ailanachim@gmail.com>
2023-11-04 18:55:05 +0300

    first try

---
+++ src/main/java/edu/hw4/Solution.java
@@ -0,0 +1,186 @@
+package edu.hw4;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Solution {
+
+    private Solution() {
+    }
+
+    // task 1
+    public static List<Animal> sortByHeight(List<Animal> animals) {
+        return animals.stream().sorted(Comparator.comparing(Animal::height)).toList();
+    }
+
+    // task 2
+    public static List<Animal> kMaxByWeight(List<Animal> animals, int k) {
+        return animals.stream().sorted(Comparator.comparing(Animal::weight).reversed()).limit(k).toList();
+    }
+
+    // task 3
+    public static Map<Animal.Type, Integer> countForEachType(List<Animal> animals) {
+        return animals.stream().collect(Collectors.groupingBy(Animal::type, Collectors.summingInt(value -> 1)));
+    }
+
+    // task 4
+    public static Animal maxByNameLength(List<Animal> animals) {
+        return animals.stream().max(Comparator.comparing(animal -> animal.name().length())).orElseThrow();
+    }
+
+    // task 5
+    public static Animal.Sex mostCommonSex(List<Animal> animals) {
+        return animals.stream().collect(Collectors.groupingBy(Animal::sex, Collectors.counting()))
+            .entrySet().stream().max(Map.Entry.comparingByValue()).orElseThrow().getKey();
+    }
+
+    // task 6
+    public static Map<Animal.Type, Optional<Animal>> maxByWeightForEachType(List<Animal> animals) {
+        return animals.stream().collect(Collectors.groupingBy(
+            Animal::type,
+            Collectors.maxBy(Comparator.comparing(Animal::weight))
+        ));
+    }
+
+    // task 7
+    public static Animal kThMaxByAge(List<Animal> animals, int k) {
+        return animals.stream().sorted(Comparator.comparing(Animal::age).reversed()).limit(k)
+            .min(Comparator.comparing(Animal::age)).orElseThrow();
+    }
+
+    // task 8
+    public static Optional<Animal> maxByWeightWithHeightLessK(List<Animal> animals, int k) {
+        return animals.stream().filter(animal -> animal.height() < k).max(Comparator.comparing(Animal::weight));
+    }
+
+    // task 9
+    public static Integer sumOfPaws(List<Animal> animals) {
+        return (int) animals.stream().collect(Collectors.summarizingInt(Animal::paws)).getSum();
+    }
+
+    // task 10
+    public static List<Animal> hasNotEqualAgeAndPaws(List<Animal> animals) {
+        return animals.stream().filter(animal -> animal.age() != animal.paws()).toList();
+    }
+
+    // task 11
+    public static List<Animal> bitesAndHigher100(List<Animal> animals) {
+        final int minHeight = 100;
+        return animals.stream().filter(animal -> animal.bites() && animal.height() > minHeight).toList();
+    }
+
+    // task 12
+    public static Integer countWhereWeightMoreHeight(List<Animal> animals) {
+        return (int) animals.stream().filter(animal -> animal.weight() > animal.height()).count();
+    }
+
+    // task 13
+    public static List<Animal> hasMoreThanTwoWordsName(List<Animal> animals) {
+        return animals.stream().filter(animal -> animal.name().split(" ").length > 2).toList();
+    }
+
+    // task 14
+    public static Boolean containsDogHigherK(List<Animal> animals, int k) {
+        return animals.stream().anyMatch(animal -> animal.type() == Animal.Type.DOG && animal.height() > k);
+    }
+
+    // task 15
+    public static Map<Animal.Type, Integer> sumOfWeightWhereAgeBetweenKLForEachType(
+        List<Animal> animals,
+        int k,
+        int l
+    ) {
+        return animals.stream().filter(animal -> animal.age() >= k && animal.age() <= l)
+            .collect(Collectors.groupingBy(Animal::type, Collectors.summingInt(Animal::weight)));
+    }
+
+    // task 16
+    public static List<Animal> sortByTypeSexName(List<Animal> animals) {
+        return animals.stream()
+            .sorted(Comparator.comparing(Animal::type).thenComparing(Animal::sex).thenComparing(Animal::name)).toList();
+    }
+
+    // task 17
+    public static Boolean spidersBiteMoreOftenThanDogs(List<Animal> animals) {
+        List<Animal> spidersAndDogs =
+            animals.stream().filter(animal -> animal.type() == Animal.Type.SPIDER || animal.type() == Animal.Type.DOG)
+                .toList();
+
+        Map<Animal.Type, Long> animalCount =
+            spidersAndDogs.stream().collect(Collectors.groupingBy(Animal::type, Collectors.counting()));
+
+        Map<Animal.Type, Long> biteAnimalCount =
+            spidersAndDogs.stream().filter(Animal::bites)
+                .collect(Collectors.groupingBy(Animal::type, Collectors.counting()));
+
+        double spiderProportion =
+            (double) biteAnimalCount.getOrDefault(Animal.Type.SPIDER, 0L)
+                / animalCount.getOrDefault(Animal.Type.SPIDER, 1L);
+
+        double dogProportion =
+            (double) biteAnimalCount.getOrDefault(Animal.Type.DOG, 0L)
+                / animalCount.getOrDefault(Animal.Type.DOG, 1L);
+
+        return spiderProportion > dogProportion;
+    }
+
+    // task 18
+    public static Animal fishWithMaxWeight(List<Animal> list1, List<Animal> list2, List<Animal>... animals) {
+        Stream<Animal> stream = Stream.concat(list1.stream(), list2.stream());
+        for (List<Animal> list : animals) {
+            stream = Stream.concat(stream, list.stream());
+        }
+
+        return stream.filter(animal -> animal.type() == Animal.Type.FISH).max(Comparator.comparing(Animal::weight))
+            .orElseThrow();
+    }
+
+    // task 19
+    public static Map<String, Set<ValidationError>> getValidationErrors(List<Animal> animals) {
+        return animals.stream().collect(Collectors.toMap(Animal::name, Solution::validateAnimal));
+    }
+
+    private static Set<ValidationError> validateAnimal(Animal animal) {
+        Set<ValidationError> errors = new HashSet<>();
+
+        if (animal == null) {
+            errors.add(new ValidationError("animal is null"));
+            return errors;
+        }
+
+        if (animal.name() == null) {
+            errors.add(new ValidationError("name is null"));
+        } else if (animal.name().isBlank()) {
+            errors.add(new ValidationError("name is blank"));
+        }
+
+        if (animal.age() < 0) {
+            errors.add(new ValidationError("age is negative"));
+        }
+
+        if (animal.height() < 0) {
+            errors.add(new ValidationError("height is negative"));
+        }
+
+        if (animal.weight() < 0) {
+            errors.add(new ValidationError("weight is negative"));
+        }
+
+        return errors;
+    }
+
+    // task 20
+    public static Map<String, String> transformErrorsToString(Map<String, Set<ValidationError>> errorMap) {
+        return errorMap.entrySet().stream().map(entry -> Map.entry(entry.getKey(), entry.getValue().stream().map(
+            ValidationError::message).reduce(((s, s2) -> s + ", " + s2)).orElse(""))).collect(Collectors.toMap(
+            Map.Entry::getKey,
+            Map.Entry::getValue
+        ));
+    }
+}
---
+++ src/test/java/edu/hw4/SolutionTest.java
@@ -0,0 +1,280 @@
+package edu.hw4;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SolutionTest {
+
+    private final Animal cat = new Animal("Barsik", Animal.Type.CAT, Animal.Sex.M, 2, 30, 3000, true);
+    private final Animal dog = new Animal("Dara", Animal.Type.DOG, Animal.Sex.F, 5, 70, 10000, true);
+    private final Animal bird = new Animal("Sparrow", Animal.Type.BIRD, Animal.Sex.M, 4, 20, 100, false);
+    private final Animal fish = new Animal("Salmon", Animal.Type.FISH, Animal.Sex.M, 3, 8, 300, false);
+    private final Animal spider = new Animal("Spider", Animal.Type.SPIDER, Animal.Sex.M, 1, 1, 10, true);
+
+    @Test
+    void task1Test() {
+        assertThat(Solution.sortByHeight(List.of(cat, dog, bird, fish, spider))).isEqualTo(List.of(
+            spider,
+            fish,
+            bird,
+            cat,
+            dog
+        ));
+    }
+
+    @Test
+    void task2Test() {
+        assertThat(Solution.kMaxByWeight(List.of(cat, dog, bird, fish, spider), 1)).isEqualTo(List.of(dog));
+        assertThat(Solution.kMaxByWeight(List.of(cat, dog, bird, fish, spider), 2)).isEqualTo(List.of(dog, cat));
+        assertThat(Solution.kMaxByWeight(List.of(cat, dog, bird, fish, spider), 3)).isEqualTo(List.of(dog, cat, fish));
+        assertThat(Solution.kMaxByWeight(List.of(cat, dog, bird, fish, spider), 4)).isEqualTo(List.of(
+            dog,
+            cat,
+            fish,
+            bird
+        ));
+        assertThat(Solution.kMaxByWeight(List.of(cat, dog, bird, fish, spider), 5)).isEqualTo(List.of(
+            dog,
+            cat,
+            fish,
+            bird,
+            spider
+        ));
+    }
+
+    @Test
+    void task3Test() {
+        Animal cat1 = new Animal("Murka", Animal.Type.CAT, Animal.Sex.F, 1, 27, 1800, true);
+        assertThat(Solution.countForEachType(List.of(cat, dog, bird, fish, spider, cat1))).isEqualTo(Map.of(
+            Animal.Type.CAT, 2,
+            Animal.Type.DOG, 1,
+            Animal.Type.SPIDER, 1,
+            Animal.Type.BIRD, 1,
+            Animal.Type.FISH, 1
+        ));
+    }
+
+    @Test
+    void task4Test() {
+        assertThat(Solution.maxByNameLength(List.of(cat, dog, bird, fish, spider))).isEqualTo(bird);
+    }
+
+    @Test
+    void task5Test() {
+        assertThat(Solution.mostCommonSex(List.of(cat, dog, bird, fish, spider))).isEqualTo(Animal.Sex.M);
+    }
+
+    @Test
+    void task6Test() {
+        Animal cat1 = new Animal("Murka", Animal.Type.CAT, Animal.Sex.F, 1, 27, 2400, true);
+        Animal dog1 = new Animal("Sharik", Animal.Type.DOG, Animal.Sex.M, 4, 60, 6500, true);
+        Animal bird1 = new Animal("Parrot", Animal.Type.BIRD, Animal.Sex.F, 10, 40, 1700, false);
+        Animal fish1 = new Animal("Goldfish", Animal.Type.FISH, Animal.Sex.F, 1, 10, 250, false);
+        Animal spider1 = new Animal("Spider2", Animal.Type.SPIDER, Animal.Sex.F, 1, 2, 15, true);
+
+        assertThat(Solution.maxByWeightForEachType(List.of(
+            cat,
+            dog,
+            bird,
+            fish,
+            spider,
+            cat1,
+            dog1,
+            bird1,
+            fish1,
+            spider1
+        ))).isEqualTo(
+            Map.of(
+                Animal.Type.CAT, Optional.of(cat),
+                Animal.Type.DOG, Optional.of(dog),
+                Animal.Type.SPIDER, Optional.of(spider1),
+                Animal.Type.BIRD, Optional.of(bird1),
+                Animal.Type.FISH, Optional.of(fish)
+            ));
+    }
+
+    @Test
+    void task7Test() {
+        assertThat(Solution.kThMaxByAge(List.of(cat, dog, bird, fish, spider), 1)).isEqualTo(dog);
+        assertThat(Solution.kThMaxByAge(List.of(cat, dog, bird, fish, spider), 2)).isEqualTo(bird);
+        assertThat(Solution.kThMaxByAge(List.of(cat, dog, bird, fish, spider), 3)).isEqualTo(fish);
+        assertThat(Solution.kThMaxByAge(List.of(cat, dog, bird, fish, spider), 4)).isEqualTo(cat);
+        assertThat(Solution.kThMaxByAge(List.of(cat, dog, bird, fish, spider), 5)).isEqualTo(spider);
+    }
+
+    @Test
+    void task8Test() {
+        assertThat(Solution.maxByWeightWithHeightLessK(
+            List.of(cat, dog, bird, fish, spider),
+            10
+        )).isEqualTo(Optional.of(fish));
+
+        assertThat(Solution.maxByWeightWithHeightLessK(
+            List.of(cat, dog, bird, fish, spider),
+            40
+        )).isEqualTo(Optional.of(cat));
+
+        assertThat(Solution.maxByWeightWithHeightLessK(
+            List.of(cat, dog, bird, fish, spider),
+            100
+        )).isEqualTo(Optional.of(dog));
+    }
+
+    @Test
+    void task9Test() {
+        assertThat(Solution.sumOfPaws(List.of(cat, dog, bird, fish, spider))).isEqualTo(18);
+    }
+
+    @Test
+    void task10Test() {
+        Animal cat1 = new Animal("Murka", Animal.Type.CAT, Animal.Sex.F, 4, 27, 2400, true);
+        assertThat(Solution.hasNotEqualAgeAndPaws(List.of(cat, dog, bird, fish, spider, cat1))).isEqualTo(List.of(
+            cat,
+            dog,
+            bird,
+            fish,
+            spider
+        ));
+    }
+
+    @Test
+    void task11Test() {
+        Animal dog1 = new Animal("Sharik", Animal.Type.DOG, Animal.Sex.M, 4, 120, 6500, true);
+        assertThat(Solution.bitesAndHigher100(List.of(cat, dog, bird, fish, spider, dog1)))
+            .isEqualTo(List.of(dog1));
+    }
+
+    @Test
+    void task12Test() {
+        Animal spider1 = new Animal("Spider2", Animal.Type.SPIDER, Animal.Sex.F, 1, 20, 15, true);
+        assertThat(Solution.countWhereWeightMoreHeight(List.of(cat, dog, bird, fish, spider, spider1))).isEqualTo(5);
+    }
+
+    @Test
+    void task13Test() {
+        Animal cat1 = new Animal("The Smelly cat", Animal.Type.CAT, Animal.Sex.F, 1, 27, 2400, true);
+        assertThat(Solution.hasMoreThanTwoWordsName(List.of(
+            cat,
+            dog,
+            bird,
+            fish,
+            spider,
+            cat1
+        ))).isEqualTo(List.of(cat1));
+    }
+
+    @Test
+    void task14Test() {
+        assertThat(Solution.containsDogHigherK(List.of(cat, dog, bird, fish, spider), 50)).isEqualTo(true);
+        assertThat(Solution.containsDogHigherK(List.of(cat, dog, bird, fish, spider), 100)).isEqualTo(false);
+    }
+
+    @Test
+    void task15Test() {
+        assertThat(Solution.sumOfWeightWhereAgeBetweenKLForEachType(List.of(cat, dog, bird, fish, spider), 2, 4))
+            .isEqualTo(Map.of(
+                    Animal.Type.CAT, cat.weight(),
+                    Animal.Type.BIRD, bird.weight(),
+                    Animal.Type.FISH, fish.weight()
+                )
+            );
+    }
+
+    @Test
+    void task16Test() {
+        Animal cat1 = new Animal("Murka", Animal.Type.CAT, Animal.Sex.F, 1, 27, 2400, true);
+        Animal dog1 = new Animal("Sharik", Animal.Type.DOG, Animal.Sex.M, 4, 60, 6500, true);
+        Animal bird1 = new Animal("Parrot", Animal.Type.BIRD, Animal.Sex.M, 10, 40, 1700, false);
+        Animal fish1 = new Animal("Goldfish", Animal.Type.FISH, Animal.Sex.F, 1, 10, 250, false);
+        Animal spider1 = new Animal("Spider2", Animal.Type.SPIDER, Animal.Sex.M, 1, 2, 15, true);
+
+        assertThat(Solution.sortByTypeSexName(
+            List.of(
+                cat,
+                dog,
+                bird,
+                fish,
+                spider,
+                cat1,
+                dog1,
+                bird1,
+                fish1,
+                spider1
+            ))).isEqualTo(List.of(
+            cat,
+            cat1,
+            dog1,
+            dog,
+            bird1,
+            bird,
+            fish,
+            fish1,
+            spider,
+            spider1
+        ));
+    }
+
+    @Test
+    void task17Test() {
+        Animal dog1 = new Animal("Sharik", Animal.Type.DOG, Animal.Sex.M, 4, 60, 6500, false);
+        Animal spider1 = new Animal("Spider2", Animal.Type.SPIDER, Animal.Sex.F, 1, 20, 15, false);
+
+        assertThat(Solution.spidersBiteMoreOftenThanDogs(List.of(cat, dog, bird, fish, spider))).isEqualTo(false);
+        assertThat(Solution.spidersBiteMoreOftenThanDogs(List.of(cat, dog, bird, fish, spider, dog1))).isEqualTo(true);
+        assertThat(Solution.spidersBiteMoreOftenThanDogs(List.of(cat, dog, bird, fish, spider, spider1))).isEqualTo(
+            false);
+    }
+
+    @Test
+    void task18Test() {
+        Animal fish1 = new Animal("Goldfish", Animal.Type.FISH, Animal.Sex.F, 1, 10, 250, false);
+        Animal fish2 = new Animal("Dori", Animal.Type.FISH, Animal.Sex.F, 3, 15, 400, false);
+
+        List<Animal> list1 = List.of(cat, dog, bird, fish, spider);
+        List<Animal> list2 = List.of();
+        List<Animal> list3 = List.of(fish1, cat, fish2);
+        List<Animal> list4 = List.of(dog);
+
+        assertThat(Solution.fishWithMaxWeight(list1, list2, list3, list4)).isEqualTo(fish2);
+    }
+
+    @Test
+    void task19Test() {
+        Animal cat1 = new Animal("Murka", Animal.Type.CAT, Animal.Sex.F, -1, 27, 2400, true);
+        Animal dog1 = new Animal("Sharik", Animal.Type.DOG, Animal.Sex.M, 4, -60, 6500, true);
+        Animal bird1 = new Animal("Parrot", Animal.Type.BIRD, Animal.Sex.M, 10, 40, -1700, false);
+        Animal fish1 = new Animal("   ", Animal.Type.FISH, Animal.Sex.F, 1, 10, 250, false);
+        Animal spider1 = new Animal("Spider2", Animal.Type.SPIDER, Animal.Sex.M, 1, 2, 15, true);
+
+        Map<String, Set<ValidationError>> errors = Map.of(
+            "Murka", Set.of(new ValidationError("age is negative")),
+            "Sharik", Set.of(new ValidationError("height is negative")),
+            "Parrot", Set.of(new ValidationError("weight is negative")),
+            "   ", Set.of(new ValidationError("name is blank")),
+            "Spider2", Set.of()
+        );
+
+        assertThat(Solution.getValidationErrors(List.of(cat1, dog1, bird1, fish1, spider1))).isEqualTo(errors);
+    }
+
+    @Test
+    void task20Test() {
+        Map<String, Set<ValidationError>> errors = Map.of(
+            "Murka", Set.of(new ValidationError("age is negative"), new ValidationError("height is negative")),
+            "Sharik", Set.of(),
+            "   ", Set.of(new ValidationError("name is blank"))
+        );
+
+        Map<String, String> result = Map.of(
+            "Murka", "age is negative, height is negative",
+            "Sharik", "",
+            "   ", "name is blank"
+        );
+
+        assertThat(Solution.transformErrorsToString(errors)).isEqualTo(result);
+    }
+}
